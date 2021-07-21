7ae716f7ff741776558f6cfb1cca1f9868ad541c
Erik Borsi <erikborsi@users.noreply.github.com>
2021-07-21 15:47:07 +0100

    one, two, and four tasks done

--- task_four.py
+++ task_four.py
@@ -1,3 +1,20 @@
 # 4)  Write a program which returns a boolean value indicating whether a given sentence is a palindrome.  A sentence
 # which contains the same character sequence when reversed (ignoring whitespace).
 # Example: "step on no pets" or "put it up".
+
+def is_palindrome(string_in):
+    string_in = string_in.replace(" ", "")
+    reversed_string = string_in[::-1]
+    if string_in == reversed_string:
+        return True
+    else:
+        return False
+
+
+print(is_palindrome("put it up"))
+print(is_palindrome("step on no pets"))
+
+# Deleted or replaced the whitespaces with the .replace()
+# Reversed the string the easiest way
+# Then compared in the conditions of an if-else statement
+# Then returned True or False accordingly.
--- task_three.py
+++ task_three.py
@@ -3,5 +3,32 @@
 # Example: `John Smith` -> `oJnhS imht`
 # Example: `Larry` => `aLrry`
 
-def reverse_string(string_in):
-    print("something")
+def check_if_even(string_in):
+    string_out = string_in
+
+    if len(string_in) % 2 == 0:
+        even(string_out)
+    else:
+        uneven(string_out)
+
+
+def even(string_in):
+    temp_list = []
+    temp_list2 = []
+
+    for i in string_in:
+        temp_list.append(i)
+
+    for j in string_in[0:len(string_in):2]:
+        temp_list2.append(j)
+        temp_list.remove(j)
+
+
+    print(temp_list)
+    print(temp_list2)
+
+def uneven(string_in):
+    print("uneven")
+
+
+check_if_even("John Smith")
